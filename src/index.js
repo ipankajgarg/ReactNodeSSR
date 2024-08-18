e3f910cc3e8d5140b9f22137d9d703af51ce46c5
ipankajgarg <pankaj.garg@viacom18.com>
2024-08-18 22:30:51 +0530

    commiting manifest and chunks

--- src/index.js
+++ src/index.js
@@ -3,74 +3,77 @@
 // const renderToString = require('react-dom/server').renderToString;
 // const Home = require('./client/components/Home').default
 
+import express from "express";
+import { matchRoutes } from "react-router-config";
+import proxy from "express-http-proxy";
+import Routes from "./client/Routes";
+import renderer from "./helpers/renderer";
 
-import express from 'express';
-import {matchRoutes} from 'react-router-config';
-import proxy from 'express-http-proxy'
-import Routes from './client/Routes'
-import renderer from './helpers/renderer'
-
-
-import createStore from './helpers/createStore'
-
-
+import createStore from "./helpers/createStore";
+var cors = require('cors')
 
 const app = express();
 
-app.use('/api',proxy("http://react-ssr-api.herokuapp.com",{
-    proxyReqOptDecorator(opts){
-        opts.headers['x-forwarded-host'] = 'localhost:3000';
-        return opts;
-    }
-}))
-app.use(express.static('public'))
 
-app.get('*',(req,res)=>{
-    const store = createStore(req)
+ 
+app.use(cors())
+
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com", {
+    proxyReqOptDecorator(opts) {
+      opts.headers["x-forwarded-host"] = "localhost:3000";
+      return opts;
+    },
+  })
+);
+app.use(express.static("public"));
+
+// app.get("*", (req, res) => {
+//   const store = createStore(req);
+
+//   console.log("print", req.path, matchRoutes(Routes, req.path));
+
+//   const promises = matchRoutes(Routes, req.path)
+//     .map(({ route }) => {
+//       return route.loadData ? route.loadData(store) : null;
+//     })
+//     .map((promise) => {
+//       if (promise) {
+//         return new Promise((resolve, reject) => {
+//           promise.then(resolve).catch(resolve);
+//         });
+//       }
+//     });
+
+//   console.log("promises", promises);
+
+//   Promise.all(promises).then(() => {
+//     const context = {};
+//     const content = renderer(req, store, context);
+
+//     if (context.url) {
+//       return res.redirect(301, context.url);
+//     }
+
+//     if (context.notFound) {
+//       res.status(404);
+//     }
+
+//     res.send(content);
+//     //res.send("<div>hello</div>")
+//   });
+// });
 
 
-console.log( "print",req.path, matchRoutes(Routes,req.path))
+app.get('/manifest',(req, res)=>{
+console.log("sending mpd")
+res.setHeader("Content-Type", "application/dash+xml");
 
-const promises = matchRoutes(Routes,req.path).map(({route})=>{
-   return route.loadData ? route.loadData(store):null
-}).map(promise=>{
-    if(promise){
+  res.download('./public/manifest.mpd')
 
-return new Promise((resolve,reject)=>{
-    promise.then(resolve).catch(resolve); 
 })
-
-    }
-})
-
-console.log("promises",promises)
-
-
-
-Promise.all(promises).then(()=>{
-
-    const context = {};
-    const content = renderer(req,store,context);
-
-if(context.url){
-    return res.redirect(301,context.url)
-}
-
-
-    if(context.notFound){
-        res.status(404)
-    }
-
-
-    res.send(content)
-    //res.send("<div>hello</div>")
-})
-
-
-
-})
-
-
-app.listen(3000,()=>{
-    console.log("Listening on port 3000")
-})
+const PORT = process.env.PORT || 5001
+app.listen(PORT, () => {
+  console.log("Listening on port"+ PORT);
+});
